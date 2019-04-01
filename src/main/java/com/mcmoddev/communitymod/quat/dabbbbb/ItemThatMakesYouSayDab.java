ebbe633827bea298956a5f0d8c8f20d9e3a5489f
itsmeow <mods.itsmeow@gmail.com>
2019-04-01 02:35:38 -0700

    Remove unused stuff from the dab-o-tron

--- src/main/java/com/mcmoddev/communitymod/quat/dabbbbb/ItemThatMakesYouSayDab.java
+++ src/main/java/com/mcmoddev/communitymod/quat/dabbbbb/ItemThatMakesYouSayDab.java
@@ -1,7 +1,6 @@
 package com.mcmoddev.communitymod.quat.dabbbbb;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -13,8 +12,7 @@ import net.minecraft.world.World;
 public class ItemThatMakesYouSayDab extends Item {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-		if(!world.isRemote && player instanceof EntityPlayerMP) {
-			EntityPlayerMP playerMP = (EntityPlayerMP) player;
+		if(!world.isRemote) {
 			String text = Math.random() >= 0.01 ? "Dab" : "Neat (is a mod by Vazkii)";
 			world.getMinecraftServer().getPlayerList().sendMessage(new TextComponentTranslation("chat.type.text", player.getName(), text));
 		}
