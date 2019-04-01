2b4d598fe798d56f9921fef0db53a01669d0ddde
its_meow <mods.itsmeow@gmail.com>
2019-03-31 23:59:30 -0700

    Make the player actually say dab (#33)
    * Make the player actually say dab
    
    * Improve upon the dab text, now including NEAT!
    
    * Remove unused stuff from the dab-o-tron

--- src/main/java/com/mcmoddev/communitymod/quat/dabbbbb/ItemThatMakesYouSayDab.java
+++ src/main/java/com/mcmoddev/communitymod/quat/dabbbbb/ItemThatMakesYouSayDab.java
@@ -6,14 +6,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
 public class ItemThatMakesYouSayDab extends Item {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-		//Someone PR this and make it betterer
-		player.sendStatusMessage(new TextComponentString("dab"), false);
+		if(!world.isRemote) {
+			String text = Math.random() >= 0.01 ? "Dab" : "Neat (is a mod by Vazkii)";
+			world.getMinecraftServer().getPlayerList().sendMessage(new TextComponentTranslation("chat.type.text", player.getName(), text));
+		}
 		return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
 	}
 }
