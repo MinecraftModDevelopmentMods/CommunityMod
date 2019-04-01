427f4fb24b483ce7d7abc417c6a356f4ad1ce5f1
Antoine G <25696312+antoinegag@users.noreply.github.com>
2019-03-31 23:07:55 -0400

    Fix global modid reference missing underscore

--- src/main/java/com/mcmoddev/communitymod/CommunityGlobals.java
+++ src/main/java/com/mcmoddev/communitymod/CommunityGlobals.java
@@ -3,6 +3,6 @@ package com.mcmoddev.communitymod;
 public class CommunityGlobals {
 	
 	public static int entity_id = 0;
-	public static final String MOD_ID = "communitymod";
+	public static final String MOD_ID = "community_mod";
 	
 }
