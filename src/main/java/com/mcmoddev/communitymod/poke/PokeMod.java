bb31ca11b3977e43dfb7c241f695de3a1bffd716
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-04-01 18:43:25 +1100

    Furnace Recipe & world gen

--- src/main/java/com/mcmoddev/communitymod/poke/PokeMod.java
+++ src/main/java/com/mcmoddev/communitymod/poke/PokeMod.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.passive.HorseArmorType;
+import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
@@ -17,11 +18,15 @@ import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -120,6 +125,44 @@ public final class PokeMod implements ISubMod {
 		return registry.getValues().stream()
 				.filter(entry -> Objects.requireNonNull(entry.getRegistryName()).getNamespace().equals(MOD_ID))
 				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void onInit(final FMLInitializationEvent event) {
+
+		GameRegistry.addSmelting(
+				Item.getItemFromBlock(POKE_ORE),
+				new ItemStack(POKE_INGOT),
+				0x1.0p0f
+		);
+
+		GameRegistry.addSmelting(
+				POKE_INGOT,
+				new ItemStack(Items.DIAMOND),
+				0x1.0p0f
+		);
+
+		GameRegistry.registerWorldGenerator(
+				(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider) -> {
+					if (world.provider.getDimension() == 0) {
+						int deltaY = 0b1000000 - 0b10000;
+
+						final PooledMutableBlockPos pos = PooledMutableBlockPos.retain();
+						try {
+							for (int i = 0; i < 0b110; ++i) {
+								pos.setPos((chunkX << 0b100) + random.nextInt(0b10000), 0b10000 + random.nextInt(deltaY), (chunkZ << 0b100) + random.nextInt(0b10000));
+
+								WorldGenMinable generator = new WorldGenMinable(POKE_ORE.getDefaultState(), 0b100 + random.nextInt(0b100));
+								generator.generate(world, random, pos);
+							}
+						} finally {
+							pos.release();
+						}
+					}
+				},
+				3
+		);
+
 	}
 
 	@Override
