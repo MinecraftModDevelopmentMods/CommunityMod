6fdf17267a83fe256a88444896e95c8c0eb6b6dc
gegy1000 <gegy1000@gmail.com>
2019-04-01 20:26:06 +0200

    You could make a religion out of this

--- src/main/java/com/mcmoddev/communitymod/shared/RegUtil.java
+++ src/main/java/com/mcmoddev/communitymod/shared/RegUtil.java
@@ -5,37 +5,47 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.Objects;
 
 public class RegUtil {
-	public static <T extends Block> T registerBlock(IForgeRegistry<Block> reg, T block, String name) {
-		block.setRegistryName(new ResourceLocation(CommunityGlobals.MOD_ID, name));
-		block.setTranslationKey(CommunityGlobals.MOD_ID + '.' + name);
-		block.setCreativeTab(CommunityGlobals.TAB);
-		
-		reg.register(block);
-		
-		return block;
-	}
-	
-	public static <T extends ItemBlock> T registerItemBlock(IForgeRegistry<Item> reg, T item) {
-		item.setRegistryName(Objects.requireNonNull(item.getBlock().getRegistryName()));
-		item.setCreativeTab(CommunityGlobals.TAB);
-		
-		reg.register(item);
-		
-		return item;
-	}
-	
-	public static <T extends Item> T registerItem(IForgeRegistry<Item> reg, T item, String name) {
-		item.setRegistryName(new ResourceLocation(CommunityGlobals.MOD_ID, name));
-		item.setTranslationKey(CommunityGlobals.MOD_ID + '.' + name);
-		item.setCreativeTab(CommunityGlobals.TAB);
-		
-		reg.register(item);
-		
-		return item;
-	}
+    public static <T extends Block> T registerBlock(IForgeRegistry<Block> reg, T block, String name) {
+        block.setRegistryName(new ResourceLocation(CommunityGlobals.MOD_ID, name));
+        block.setTranslationKey(CommunityGlobals.MOD_ID + '.' + name);
+        block.setCreativeTab(CommunityGlobals.TAB);
+
+        reg.register(block);
+
+        return block;
+    }
+
+    public static <T extends ItemBlock> T registerItemBlock(IForgeRegistry<Item> reg, T item) {
+        item.setRegistryName(Objects.requireNonNull(item.getBlock().getRegistryName()));
+        item.setCreativeTab(CommunityGlobals.TAB);
+
+        reg.register(item);
+
+        return item;
+    }
+
+    public static <T extends Item> T registerItem(IForgeRegistry<Item> reg, T item, String name) {
+        item.setRegistryName(new ResourceLocation(CommunityGlobals.MOD_ID, name));
+        item.setTranslationKey(CommunityGlobals.MOD_ID + '.' + name);
+        item.setCreativeTab(CommunityGlobals.TAB);
+
+        reg.register(item);
+
+        return item;
+    }
+
+    public static SoundEvent registerSound(IForgeRegistry<SoundEvent> reg, ResourceLocation name) {
+        SoundEvent event = new SoundEvent(name);
+        event.setRegistryName(name);
+
+        reg.register(event);
+
+        return event;
+    }
 }
