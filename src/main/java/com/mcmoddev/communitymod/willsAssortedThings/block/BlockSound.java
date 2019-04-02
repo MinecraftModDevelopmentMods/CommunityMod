84448e0d4a93d07f7c5e63b13726d6959ce80f79
williambl <william@williambl.com>
2019-04-02 19:04:03 +0100

    Add Bruh Sound Effect #2 Block

--- src/main/java/com/mcmoddev/communitymod/willsAssortedThings/block/BlockSound.java
+++ src/main/java/com/mcmoddev/communitymod/willsAssortedThings/block/BlockSound.java
@@ -5,14 +5,15 @@ import com.mcmoddev.communitymod.willsAssortedThings.sound.ModSounds;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.SoundEvent;
 
-public class BlockE extends Block {
+public class BlockSound extends Block {
 
-    public BlockE() {
+    public BlockSound(String name, SoundEvent sound) {
         super(Material.ROCK);
         this.setCreativeTab(CommunityGlobals.TAB);
-        this.setSoundType(new SoundType(2.0F, 1.0F, ModSounds.E_SOUND, ModSounds.E_SOUND, ModSounds.E_SOUND, ModSounds.E_SOUND, ModSounds.E_SOUND));
-        this.setRegistryName("e_block");
+        this.setSoundType(new SoundType(2.0F, 1.0F, sound, sound, sound, sound, sound));
+        this.setRegistryName(name);
         this.setTranslationKey(getRegistryName().toString());
     }
 
