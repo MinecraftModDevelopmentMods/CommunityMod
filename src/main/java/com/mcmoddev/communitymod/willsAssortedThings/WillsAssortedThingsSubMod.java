f83779f43a8cd1e510d2176beba76346bead0f1a
williambl <william@williambl.com>
2019-10-02 20:30:44 +0100

    Add Chicken Arrow Entity

--- src/main/java/com/mcmoddev/communitymod/willsAssortedThings/WillsAssortedThingsSubMod.java
+++ src/main/java/com/mcmoddev/communitymod/willsAssortedThings/WillsAssortedThingsSubMod.java
@@ -1,15 +1,25 @@
 package com.mcmoddev.communitymod.willsAssortedThings;
 
+import com.mcmoddev.communitymod.CommunityGlobals;
 import com.mcmoddev.communitymod.ISubMod;
 import com.mcmoddev.communitymod.SubMod;
 import com.mcmoddev.communitymod.willsAssortedThings.block.ModBlocks;
+import com.mcmoddev.communitymod.willsAssortedThings.client.render.RenderChickenArrow;
+import com.mcmoddev.communitymod.willsAssortedThings.entity.EntityChickenArrow;
+import com.mcmoddev.communitymod.willsAssortedThings.entity.ModEntities;
 import com.mcmoddev.communitymod.willsAssortedThings.item.ModItems;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderArrow;
+import net.minecraft.client.renderer.entity.RenderChicken;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.UUID;
@@ -48,6 +58,17 @@ public class WillsAssortedThingsSubMod implements ISubMod {
     }
 
     @Override
+    public void registerEntities(IForgeRegistry<EntityEntry> reg) {
+        reg.registerAll(EntityEntryBuilder.create()
+                .entity(EntityChickenArrow.class)
+                .factory(EntityChickenArrow::new)
+                .id(new ResourceLocation(CommunityGlobals.MOD_ID, "chicken_arrow"), CommunityGlobals.entity_id++)
+                .name("chicken_arrow")
+                .tracker(64, 3, true)
+                .build());
+    }
+
+    @Override
     public void registerModels(ModelRegistryEvent event) {
        for (Block block : ModBlocks.blocks) {
             simpleItemModel(Item.getItemFromBlock(block));
@@ -55,5 +76,6 @@ public class WillsAssortedThingsSubMod implements ISubMod {
        for (Item item : ModItems.items) {
            simpleItemModel(item);
        }
+        RenderingRegistry.registerEntityRenderingHandler(EntityChickenArrow.class, RenderChickenArrow::new);
     }
 }
---
+++ src/main/java/com/mcmoddev/communitymod/willsAssortedThings/entity/ModEntities.java
@@ -0,0 +1,15 @@
+package com.mcmoddev.communitymod.willsAssortedThings.entity;
+
+import com.mcmoddev.communitymod.CommunityGlobals;
+import com.mcmoddev.communitymod.CommunityMod;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(CommunityGlobals.MOD_ID)
+public class ModEntities {
+
+    @GameRegistry.ObjectHolder("chicken_arrow")
+    public static EntityEntry CHICKEN_ARROW = null;
+
+}
